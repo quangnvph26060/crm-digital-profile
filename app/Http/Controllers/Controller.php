27cbb4e80bb6dec23ef0420554348feedb156918
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-10-22 09:23:38 +0700

    update code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,23 +8,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Schema;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-   public function __construct()
-   {
-   
-//         $tableName = 'phong';
-//         $columns = Schema::getColumnListing($tableName);
-        
-//         $columnDetails = [];
-//         foreach ($columns as $column) {
-//             $columnDetails[$column] = Schema::getColumnType($tableName, $column);
-//         }
-    
-//         dd($columnDetails);
-    // $activeColumns = PermissionsProfile::where('active', true)->get();
-    // dd($activeColumns);
+    public function __construct()
+    {
+
+        //         $tableName = 'phong';
+        //         $columns = Schema::getColumnListing($tableName);
+
+        //         $columnDetails = [];
+        //         foreach ($columns as $column) {
+        //             $columnDetails[$column] = Schema::getColumnType($tableName, $column);
+        //         }
+
+        //         dd($columnDetails);
+        // $activeColumns = PermissionsProfile::where('active', true)->get();
+        // dd($activeColumns);
     }
-   
 }
