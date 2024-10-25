20f881764dd667b7f110d4b7726c16be3409707a
thangngo.dev <thangngo130802@gmail.com>
2024-10-25 16:44:49 +0700

    update_export_VB

--- app/Models/array_vanban.php
+++ app/Models/array_vanban.php
@@ -9,12 +9,10 @@ return array (
   5 => 'so_va_ki_hieu_van_ban',
   6 => 'ngay_thang_van_ban',
   7 => 'tac_gia_van_ban',
-  8 => 'author',
   9 => 'trich_yeu_noi_dung_van_ban',
   10 => 'to_so',
   11 => 'ghi_chu',
   12 => 'duong_dan',
-  13 => 'filepdf',
   14 => 'profile_id',
   15 => 'status',
 );
