ae1d7db0d2a9582dc264b4e24447a243db33fa96
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-10-29 16:21:33 +0700

    update code

--- resources/views/admins/pages/profiles/index.blade.php
+++ resources/views/admins/pages/profiles/index.blade.php
@@ -308,6 +308,52 @@
 
    
 @endsection
-@section('scripts')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#column-select').on('change', function() {
+        var selectedColumns = $(this).val() || [];
+
+        // Ẩn tất cả các cột trước khi hiển thị các cột đã chọn
+        $('#userTable th, #userTable td').addClass('hidden');
+
+        // Hiển thị các cột đã chọn
+        selectedColumns.forEach(function(column) {
+            $('#userTable .column-' + column).removeClass('hidden');
+        });
+    });
+});
+
+$(document).ready(function() {
+    $('#exportExcelBtn').on('click', function() {
+        $('<input type="file">').change(function() {
+            var selectedFile = this.files[0];
+            console.log('File đã chọn:', selectedFile);
+
+
+            var formData = new FormData();
+            formData.append('file', selectedFile);
+            var url = "{{ route('import') }}";
+
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    console.log('Kết quả:', response);
+
+                },
+                error: function(xhr, status, error) {
+                    console.error('Đã xảy ra lỗi khi gửi file.');
+                }
+            });
+        }).click();
+    });
+});
+
+</script>
+{{-- @section('scripts')
     <script src="{{ asset('js/profiles.js') }}"></script>
-@endsection
+@endsection --}}
