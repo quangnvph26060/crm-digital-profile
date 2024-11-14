7458dd4984c834d25746fcc3a8e0db66763d2f45
thangngo.dev <thangngo130802@gmail.com>
2024-11-14 10:33:49 +0700

    update

--- app/Imports/InformationVbImport.php
+++ app/Imports/InformationVbImport.php
@@ -26,7 +26,7 @@ class InformationVbImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         //  dd($row);
-
+        // dd($row);
         try {
             $coquan = Config::where('agency_code', trim(str_replace(["\n", "\r"], '', $row['ma_co_quan'])))->first();
             if ($coquan) {
@@ -40,7 +40,7 @@ class InformationVbImport implements ToModel, WithHeadingRow
                     $profile = Profile::where('config_id', $coquan->id)->where('ma_muc_luc', $mucluc->id)
                         ->where('hop_so', $hopso->id)->where('ho_so_so', $row['ho_so_so'])->first();
                     if ($profile) {
-                        $vanban = InformationVb::where('so_va_ki_hieu_van_ban', $row['so_va_ki_hieu_van_ban'])->where('ma_phong', $phong->id)->where('profile_id', $profile->id)->first();
+                        $vanban = InformationVb::where('so_van_ban', $row['so_van_ban'])->where('ma_phong', $phong->id)->where('profile_id', $profile->id)->first();
                         // dd($vanban);
                         if (!$vanban) {
 
@@ -50,21 +50,24 @@ class InformationVbImport implements ToModel, WithHeadingRow
                             $vanbannew->ma_mucluc = $mucluc->id;
                             $vanbannew->hop_so = $hopso->id;
                             $vanbannew->ho_so_so = $row['ho_so_so'];
-                            $vanbannew->so_va_ki_hieu_van_ban = $row['so_va_ki_hieu_van_ban'];
+                            $vanbannew->stt = $row['stt'];
+                            $vanbannew->so_van_ban = $row['so_van_ban'];
+                            $vanbannew->ky_hieu_van_ban = $row['ky_hieu_van_ban'];
                             $vanbannew->profile_id = $profile->id;
+                            $vanbannew->ngay_thang_van_ban = Carbon::createFromFormat('d/m/Y', $row['ngay_thang_van_ban'])->format('Y-m-d');
                             foreach ($row as $key => $value) {
 
-                                if (!Schema::hasColumn('information_vb', $key) || $key == 'ma_co_quan' || $key == 'ma_phong' || $key == 'ma_mucluc' || $key == 'hop_so' || $key == 'ho_so_so' || $key == 'so_va_ki_hieu_van_ban') {
+                                if (!Schema::hasColumn('information_vb', $key) || $key == 'ma_co_quan' || $key == 'ma_phong' || $key == 'ma_mucluc' || $key == 'hop_so' || $key == 'ho_so_so' ||$key == 'stt' || $key == 'so_van_ban' || $key == 'ky_hieu_van_ban' || $key == 'ngay_thang_van_ban') {
                                     continue;
                                 }
                                 $vanbannew->$key = $value;
-                                Log::info($key);
-                                if($key == 'ngay_thang_van_ban'){
-                                    $vanbannew->$key = Carbon::createFromFormat('d/m/Y', $row[$key])->format('Y-m-d');
-                                }
+                                // Log::info($key);
+                                // if($key == 'ngay_thang_van_ban'){
+                                //     $vanbannew->$key = Carbon::createFromFormat('d/m/Y', $row[$key])->format('Y-m-d');
+                                // }
 
 
-                                $localPath = $row['duong_dan'];
+                                $localPath = $row['duong_dan_file'];
                                 if (file_exists($localPath)) {
                                     // Tạo tên file duy nhất
                                     $fileName = time() . '/' . $row['ma_co_quan'] . '/' . $row['ma_phong'] . '/' . $row['ma_muc_luc'] . '/' . $row['hop_so'] . '/' . $row['ho_so_so'] . '-' . basename($localPath);
