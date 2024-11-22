bbe4b0aae26bbd7366d9340dd0f5fac11af1f504
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-11-22 16:55:20 +0700

    update code

--- app/Imports/ProfileImport.php
+++ app/Imports/ProfileImport.php
@@ -7,18 +7,19 @@ use App\Models\HopSoModel;
 use App\Models\MucLuc;
 use App\Models\Phong;
 use App\Models\Profile;
-
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 
-class ProfileImport implements ToModel, WithHeadingRow
+class ProfileImport implements ToModel, WithHeadingRow, WithCalculatedFormulas
 {
     public function model(array $row)
     {
         try {
-            //  \Log::info($row);
             $existingCoQuan = Config::where('agency_code', $row['ma_co_quan'])->first();
 
             if (!$existingCoQuan) {
@@ -44,17 +45,17 @@ class ProfileImport implements ToModel, WithHeadingRow
                     $existingHopSo->coquan_id   =  $existingCoQuan->id;
                     $existingHopSo->phong_id    =  $existingPhong->id;
                     $existingHopSo->mucluc_id    = $existingMucLuc->id;
-                    $existingHopSo->hop_so    = $row['hop_so'];
+                    $existingHopSo->hop_so    = (substr($row['hop_so'], 0, 1) === '=') ? "'" . $row['hop_so'] : $row['hop_so'];
                     $existingHopSo->save();
 
                     //    \Log::info('mã cơ quan chưa tồn tại');
                 }
             } else {
-                //  \Log::info('mã cơ quan tồn tại');
+                  //Log::info('mã cơ quan tồn tại');
                 $phongFind = Phong::where('coquan_id', $existingCoQuan->id)->where('ma_phong', $row['ma_phong'])->first();
                 // \Log::info($row['ma_phong']);
                 if (!$phongFind) {
-                    //  \Log::info('mã cơ quan tồn tại nhưng mã phông không tồn tại');
+                      //Log::info('mã cơ quan tồn tại nhưng mã phông không tồn tại');
                     $existingPhong = new Phong();
                     $existingPhong->ten_phong   = $row['ma_phong'];
                     $existingPhong->ma_phong    = $row['ma_phong'];
@@ -71,11 +72,11 @@ class ProfileImport implements ToModel, WithHeadingRow
                     $existingHopSo->coquan_id   =  $existingCoQuan->id;
                     $existingHopSo->phong_id    =  $existingPhong->id;
                     $existingHopSo->mucluc_id    = $existingMucLuc->id;
-                    $existingHopSo->hop_so    = $row['hop_so'];
+                    $existingHopSo->hop_so    = (substr($row['hop_so'], 0, 1) === '=') ? "'" . $row['hop_so'] : $row['hop_so'];
                     $existingHopSo->save();
 
                 } else {
-                   //   \Log::info('phong ton tai');
+                     // Log::info('phong ton tai');
                     $mucluc  = MucLuc::where('phong_id', $phongFind->id)->where('ma_mucluc', $row['ma_muc_luc'])->first();
                     if (!$mucluc) {
                         //   \Log::info(' mã mục lục không tồn tại ');
@@ -89,18 +90,18 @@ class ProfileImport implements ToModel, WithHeadingRow
                         $existingHopSo->coquan_id   =  $existingCoQuan->id;
                         $existingHopSo->phong_id    =  $phongFind->id;
                         $existingHopSo->mucluc_id    = $existingMucLuc->id;
-                        $existingHopSo->hop_so    = $row['hop_so'];
+                        $existingHopSo->hop_so    = (substr($row['hop_so'], 0, 1) === '=') ? "'" . $row['hop_so'] : $row['hop_so'];
                         $existingHopSo->save();
                          
                         $existingHoSoQuery = false;
 
 
                     } else {
-                      //  \Log::info('mucluc ton tai');
+                        //Log::info('mucluc ton tai');
                         $existingHopSo  = HopSoModel::where('coquan_id', $existingCoQuan->id)
                                         ->where('phong_id', $phongFind->id)
                                         ->where('mucluc_id', $mucluc->id)
-                                        ->where('hop_so',$row['hop_so'])->first();
+                                        ->where('hop_so',(substr($row['hop_so'], 0, 1) === '=') ? "'" . $row['hop_so'] : $row['hop_so'])->first();
                                        
                         if(!$existingHopSo){
                             //  \Log::info(' hộp số không tồn tại');
@@ -108,10 +109,10 @@ class ProfileImport implements ToModel, WithHeadingRow
                             $existingHopSo->coquan_id       = $existingCoQuan->id;
                             $existingHopSo->phong_id        = $phongFind->id;
                             $existingHopSo->mucluc_id       = $mucluc->id;
-                            $existingHopSo->hop_so          = $row['hop_so'];
+                            $existingHopSo->hop_so          = (substr($row['hop_so'], 0, 1) === '=') ? "'" . $row['hop_so'] : $row['hop_so'];
                             $existingHopSo->save();
                         }else{
-                            //   \Log::info(' hộp số tồn tại');
+                               //Log::info(' hộp số tồn tại');
                           
                             if ($existingCoQuan && $phongFind && $mucluc && $existingHopSo) {
                               
@@ -135,15 +136,36 @@ class ProfileImport implements ToModel, WithHeadingRow
             // if(!$existingHoSoQuery){
             //     return;
             // }
-            if (array_key_exists('ngay_thang_bd_kt', $row)) {
-                $ngay_thang_arr = explode('-', $row['ngay_thang_bd_kt']);
-            } else {
-                $ngay_thang_arr = explode('-', $row['ngay_thang_bd']);
-            }
+            // if (array_key_exists('ngay_thang_bd_kt', $row)) {
+            //     $ngay_thang_arr = explode('-', $row['ngay_thang_bd_kt']);
+            // } else {
+            //     $ngay_thang_arr = explode('-', $row['ngay_thang_bd']);
+            // }
 
-            if (count($ngay_thang_arr) === 2) {
+            if (array_key_exists('ngay_thang_bd_kt', $row)) {
+                $dateString = $row['ngay_thang_bd_kt'];
+            } else {
+                $dateString = $row['ngay_thang_bd'];
+            }
+         
+            if (strpos($dateString, '-') !== false) {
+                $ngay_thang_arr = explode('-', $dateString);
+            }else{
+                if (is_numeric($dateString)) {
+                    $ngay_thang  = Carbon::createFromFormat('Y-m-d', \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['ngay_thang_bd_kt'])->format('Y-m-d'))->format('d/m/Y');
+                } else {
+                    $ngay_thang = $row['ngay_thang_bd_kt']; // Nếu không phải số, coi như đã là ngày tháng hợp lệ
+                }
+                $ngay_thang_arr = [$ngay_thang];
+            }
+            
+            if (is_array($ngay_thang_arr)) {
                 $ngay_bat_dau = date_create_from_format('d/m/Y', trim($ngay_thang_arr[0]));
-                $ngay_ket_thuc = date_create_from_format('d/m/Y', trim($ngay_thang_arr[1]));
+                if(count($ngay_thang_arr) === 2){
+                    $ngay_ket_thuc = date_create_from_format('d/m/Y', trim($ngay_thang_arr[1]));
+                }else{
+                    $ngay_ket_thuc = date_create_from_format('d/m/Y', trim($ngay_thang_arr[0]));
+                }
                 Profile::unguard();
                 $data = [
                     'config_id'        => $existingCoQuan->id,
@@ -184,7 +206,7 @@ class ProfileImport implements ToModel, WithHeadingRow
             }
         } catch (\Exception $e) {
             // Bắt lỗi và ghi log dòng gây ra lỗi
-            \Log::error('Lỗi trong hàm model: ' . $e->getMessage() . ' at line ' . $e->getLine());
+            Log::error('Lỗi trong hàm model: ' . $e->getMessage() . ' at line ' . $e->getLine());
         }
     }
 }
